ddea6740108689918868df160ea9c4ee14a9025b
voidGulps <rajanpeter211997@gmail.com>
2021-10-29 01:15:51 +0530

    router

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const passport = require('passport')
-const router = express.Router()
+var router = express.Router()
 
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
