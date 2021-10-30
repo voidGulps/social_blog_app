22e79a376f04ad6f1056b8b5039d5e8678e28669
voidGulps <rajanpeter211997@gmail.com>
2021-10-30 23:59:06 +0530

    lean fn

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/',ensureGuest, (req, res) => {
 
 router.get('/dashboard',ensureAuth, async(req, res) => {
     try{
-        const userArticles=await Article.find({user:req.user.id})
+        const userArticles=await Article.find({ user: req.user.id }).lean()
         res.render('dashboard',{name:req.user.firstName,userArticles})
     }
     catch(err){console.error(err)}
